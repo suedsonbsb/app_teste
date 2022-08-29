88f0e1edabcb684e697995ff83259be8bcc52dfc
suedsonbsb <82533498+suedsonbsb@users.noreply.github.com>
2022-08-28 22:32:15 -0300

    Update startNode.sh

--- startNode.sh
+++ startNode.sh
@@ -1,5 +1,5 @@
 NODEJS_HOME=/opt/nodejs
 
-echo "######### INICIALIZANDO NODEJS ##########"
+echo "########## INICIALIZANDO NODEJS ###########"
 
 pm2 start -x $NODEJS_HOME/server/src/app.js --no-daemon
